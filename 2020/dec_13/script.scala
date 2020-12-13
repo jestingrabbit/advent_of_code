da9754c1727652ac07cbbdb7fab779caeff08abb
jestingrabbit <jestingrabbit@gmail.com>
2020-12-14 03:22:53 +1100

    refactor to a single positive mod function

--- 2020/dec_13/script.scala
+++ 2020/dec_13/script.scala
@@ -5,27 +5,25 @@ import scala.math
 
 val lines = Source.fromFile("input.txt").mkString.split("\n")
 
+def positiveMod(n: Int, b: Int): Int =  b * math.ceil(n.toFloat / b).toInt - n
+
 val readyAt = lines(0).toInt
 
 val buses = lines(1).split(",").filter(_ != "x").map(_.toInt)
 
-val (waitTime, bus) = buses.map(b => (b - (readyAt % b), b)).min
+val (waitTime, bus) = buses.map(b => (positiveMod(readyAt, b), b)).min
 
 println(waitTime * bus)
 
 val busesAndOffsets = lines(1).split(",").zipWithIndex.filter(_._1 != "x").map(pr => {
   val i = pr._1.toInt
-  (i, (i - (pr._2 % i)) % i)
+  (i, positiveMod(pr._2, i))
 })
-
-busesAndOffsets.foreach(println(_))
 
 val answer2 = busesAndOffsets.foldLeft((1L, 0L))((pr1, pr2) => {
   val (b1, m1) = pr1
   val (b2, m2) = pr2
-  println(pr1)
-  // (0 until b2).foreach(a => println((a * b1 + m1) % b2))
-  ((b1*b2).toLong, (0 until b2).map(_ * b1 + m1).find(_ % b2 == m2).get)
+  (b1*b2, (0 until b2).map(_ * b1 + m1).find(_ % b2 == m2).get)
 })
 
 println(answer2._2)
